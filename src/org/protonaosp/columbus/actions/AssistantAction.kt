372407a7680ea7b9872e3cd7b13ed7c9494e5f6b
NurKeinNeid <nurkeinneid@derpfest.org>
2025-09-02 01:36:11 +0200

    AssistantAction: Gracefully handle failures
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/org/protonaosp/columbus/actions/AssistantAction.kt
+++ src/org/protonaosp/columbus/actions/AssistantAction.kt
@@ -8,14 +8,25 @@ package org.protonaosp.columbus.actions
 
 import android.content.Context
 import android.os.Bundle
+import android.os.RemoteException
 import android.os.ServiceManager
+import android.util.Log
 import com.android.internal.statusbar.IStatusBarService
+import org.protonaosp.columbus.TAG
 
 class AssistantAction(context: Context) : Action(context) {
     val service =
         IStatusBarService.Stub.asInterface(ServiceManager.getService(Context.STATUS_BAR_SERVICE))
 
     override fun run() {
-        service.startAssist(Bundle())
+        try {
+            service.startAssist(Bundle())
+        } catch (e: RemoteException) {
+            Log.e(TAG, "Failed to start assistant: service unavailable", e)
+        } catch (e: SecurityException) {
+            Log.e(TAG, "Failed to start assistant: permission denied", e)
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to start assistant: unexpected error", e)
+        }
     }
 }
